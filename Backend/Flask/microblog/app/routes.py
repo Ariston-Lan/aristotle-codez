7a98dc0cf9639e17e1a16d9bac2e43467032f185
Ariston Lanier <aristonlanier@gmail.com>
2026-07-22 21:17:43 -0700

    Took notes on CSS, Python, and Flask. In Python I did a workshop making a singe link list and in Flask I connected my URL routes to html files and used template inheritance via flask's use of Jinji

--- Backend/Flask/microblog/app/routes.py
+++ Backend/Flask/microblog/app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Maddi'}
+    posts = [
+        {
+            'author': {'username':'Ariston'},
+            'body': 'I love gay people'
+        },
+        {
+            'author': {'username':'Hect0r'},
+            'body': 'I love USCD'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
